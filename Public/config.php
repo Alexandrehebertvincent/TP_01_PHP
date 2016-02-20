f0daf7b359bc080fe80fd2d59449f4a1ba2989d7
alexandre_hebert <alexandre.hebertvincent@gmail.com>
2016-02-20 12:37:48 -0500

    Création du fichier de config de la BD

---
+++ Public/config.php
@@ -0,0 +1,16 @@
+<?php
+
+// Paramètres de connexion à la base de données MySQL
+// ==================================================
+// Serveur
+$dbHote = "97.74.233.135";
+
+// Nom d'utilisateur
+$dbUtilisateur = "garneau";
+
+// Mot de passe
+$dbMotPasse = "qwerty123";
+
+// Base de données
+$dbNom = "tp1_dionlaflamme_hebertvincent";
+?>
