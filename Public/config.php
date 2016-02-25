253060d4f715d953e1404a68f8390d590006cf1f
alexandre_hebert <alexandre.hebertvincent@gmail.com>
2016-02-25 09:45:10 -0500

    Modif du config et ajout de l'index

--- Public/config.php
+++ Public/config.php
@@ -2,6 +2,7 @@
 
 // Paramètres de connexion à la base de données MySQL
 // ==================================================
+
 // Serveur
 $dbHote = "97.74.233.135";
 
@@ -13,3 +14,11 @@ $dbMotPasse = "qwerty123";
 
 // Base de données
 $dbNom = "tp1_dionlaflamme_hebertvincent";
+
+// Création d'une connexion à la BD.
+try {
+    $connBD = new PDO('mysql:host='.$dbHote.'; dbname='.$dbNom.'', $dbUtilisateur, $dbMotPasse, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+    $connBD -> setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+} catch (PDOException $e) {
+    exit( "Erreur lors de la connexion à la BD :<br />\n" .  $e->getMessage() );
+}
