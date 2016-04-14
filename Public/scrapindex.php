8576ab4a1f2fa62d878299d973ec8ef003bfd8c4
Pasqual24 <Pasqual24@live.ca>
2016-04-13 21:17:34 -0400

    ok...?

--- Public/scrapindex.php
+++ Public/scrapindex.php
@@ -40,10 +40,43 @@ if (isset($_SESSION["messages"])) {
             <span id="fleche-out"></span>
         </header>
         <div id="page">
-            <?php require("include/affichage-films.php"); ?>
+		
+		<!-- Visionneuse -->
+		<div id="id="visionneuse">
+			<div>
+				<img id="imgvisionneuse" src="images/diapo0.jpg" alt="Le bateau"  />
+				<p id="imgText">Le bateau.</p>
+			</div>
+			<div id="boutons">
+				<button id="btnPrec">Précédent</button>
+				<button id="btnSuiv">Suivant</button>
+			</div>
+		</div>
+		
+		<ul id="liste-vignettes">
+			<li><img id="imgli1" src="" height="60" width="80" /></li>
+			<li><img id="imgli2" src="" height="60" width="80" /></li>
+			<li><img id="imgli3" src="" height="60" width="80" /></li>
+			<li><img id="imgli4" src="" height="60" width="80" /></li>
+			<li><img id="imgli5" src="" height="60" width="80" /></li>
+			
+			<!-- <li id="img2">Image 2</li>
+			<li id="img3">Image 3</li>
+			<li id="img4">Image 4</li>
+			<li id="img5">Image 5</li> -->
+		</ul>
+    </div>
+	
+			
+		<!-- Affichage des films --> 
+        <?php require("include/affichage-films.php"); ?>
+			
+			
+			
         </div>
     </section>
     <?php include "include/footer.php"; ?>
+	<script src="js/visionneuse-tp.js"></script>
 	<script class="jsbin" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 <script>
 	$(document).ready(function(){
